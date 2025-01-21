869727f5ae0d4a85f13b52f440d1186744aab7c4
Stefano Mercadante <stefmercadante@gmail.com>
2025-01-21 17:52:25 +0100

    fix

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,16 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
-// const blog = defineCollection({
-//   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
-//   schema: z.object({
-//     title: z.string(),
-//     description: z.string(),
-//     date: z.coerce.date(),
-//     draft: z.boolean().optional(),
-//     tags: z.array(z.string()).optional(),
-//   }),
-// });
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.coerce.date(),
+    draft: z.boolean().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
 
 const projects = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/projects" }),
@@ -80,7 +80,7 @@ const godot2dtopdowntemplate = defineCollection({
 });
 
 export const collections = {
-  // blog,
+  blog,
   projects,
   resources,
   assets,
